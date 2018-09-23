c938248d9e5c37528ebf387d238d372741af00c4
Peter Hofmann <peter@envato.com>
2018-09-24 09:43:17 +1000

    fix bastion bug when using SCP

--- lib/knuckle_cluster/scp.rb
+++ lib/knuckle_cluster/scp.rb
@@ -63,7 +63,11 @@ module KnuckleCluster
       ip = bastion ? agent.private_ip : agent.public_ip
       command = "scp"
       command += " -i #{rsa_key_location}" if rsa_key_location
-      command += " -o ProxyCommand='ssh -qxT #{bastion} nc #{ip} 22'" if bastion
+      if bastion.is_a? String
+        command += " -o ProxyCommand='ssh -qxT #{bastion} nc #{ip} 22'"
+      elsif bastion.is_a? Hash
+        command += " -o Proxycommand='ssh -qxt #{bastion[:host]} -l#{bastion[:username]} -i #{bastion[:rsa_key_location]} nc #{ip} 22'"
+      end
       command
     end
 
--- lib/knuckle_cluster/version.rb
+++ lib/knuckle_cluster/version.rb
@@ -1,3 +1,3 @@
 module KnuckleCluster
-  VERSION = '2.3.0'
+  VERSION = '2.3.1'
 end
